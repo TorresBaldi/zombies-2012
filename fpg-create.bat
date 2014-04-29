5203205d474cd13ec6639cbf0fa91c900d992913
Tehuel Torres Baldi <torresbaldi.tehuel@gmail.com>
2014-04-29 09:04:55 -0300

    divided scripts

---
+++ fpg-create.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+SET BINPATH=D:\bennu\bin\windows
+
+:: Compilo FPG Tools
+%BINPATH%\bgdc.exe tools\fpg-tools\fpg-tools.prg
+CLS
+
+:: 'Compilo' FPGs
+::%BINPATH%\bgdi.exe tools\fpg-tools\fpg-tools.dcb -c fpg-sources fpg 16
--- zombies.bat
+++ zombies.bat
@@ -2,16 +2,6 @@
 
 SET BINPATH=D:\bennu\bin\windows
 
-:: Compilo FPG Tools
-%BINPATH%\bgdc.exe tools\fpg-tools\fpg-tools.prg
-CLS
-
-:: 'Exporto' FPGs
-::%BINPATH%\bgdi.exe tools\fpg-tools\fpg-tools.dcb -e fpg-sources fpg 16
-
-:: 'Compilo' FPGs
-::%BINPATH%\bgdi.exe tools\fpg-tools\fpg-tools.dcb -c fpg-sources fpg 16
-
 :: Compilo juego
 %BINPATH%\bgdc.exe main.prg
 IF NOT ERRORLEVEL 2 %BINPATH%\bgdi.exe main.dcb
