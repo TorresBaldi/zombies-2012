a8d32d424e46bbf747ed0edb9cf533592ae21604
Tehuel <torresbaldi.tehuel@gmail.com>
2017-10-25 23:17:08 -0300

    updated build script

--- build.sh
+++ build.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
+cd "${0%/*}"
+
+# get bennu binaries directory parameter, or set default
+BGD_RUNTIME=${1:-"tools/bennugd-binaries/linux"}
+
+# add bennugd to system path
+export PATH=$BGD_RUNTIME/bin:$PATH
+export LD_LIBRARY_PATH=$BGD_RUNTIME/lib:$LD_LIBRARY_PATH
 
 # build bgd-fpgtool
-cd tools/bgd-fpgtool/
-./build.sh
-cd ../..
+tools/bgd-fpgtool/build.sh "../bennugd-binaries/linux"
 
 # build fpg files
-bgdc tools/bgd-fpgtool/fpgtools.prg
 bgdi tools/bgd-fpgtool/fpgtools.dcb -c fpg-sources fpg 16
 
+# export fpg files
+#bgdi tools/bgd-fpgtool/fpgtools.dcb -e fpg-out fpg 16
 
 # compile game in debug mode
 bgdc -g -D DEBUG "main.prg"
-
-# bgdi "main.dcb"
+bgdi "main.dcb"
 
 
 # invert output status of bgdc
